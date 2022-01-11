377e4713c24a5775a560f0ef40019d8059f441fb
Winnie Chien <winnie145ch@gmail.com>
2022-01-11 15:42:06 +0800

    container_edit

--- container/container_edit_api.php
+++ container/container_edit_api.php
@@ -8,6 +8,7 @@ $output = [
     'error' => '',
 ];
 
+$container_id = isset($_POST['container_id']) ? intval($_POST['container_id']) : 0;
 $container_img = $_POST['container_img'] ?? '';
 $container_shadow = $_POST['container_shadow'] ?? '';
 $container_name = $_POST['container_name'] ?? '';
@@ -39,25 +40,25 @@ if (!empty($_FILES['container_img'])) {
                 $target02 = $upload_folder . "\\" . $filename02;
                 if (move_uploaded_file($_FILES['container_img']['tmp_name'], $target01)) {
                     if (move_uploaded_file($_FILES['container_shadow']['tmp_name'], $target02)) {
-            
-                        $sql = "INSERT INTO `product_container`(
-                            `container_name`,
-                            `container_img`,
-                           `container_shadow`
-                           ) VALUES (?,?,?)";
+
+                        $sql = "UPDATE `product_container` SET
+                            `container_name`=?,
+                            `container_img`=?,
+                           `container_shadow`=?
+                            WHERE container_id=?";
                         $stmt = $pdo->prepare($sql);
                         $stmt->execute([
-                           $container_name,
-                           $filename01,
-                           $filename02
+                            $container_name,
+                            $filename01,
+                            $filename02,
+                            $container_id
                         ]);
-                        $output['success'] = $stmt->rowCount() == 1;
-                        $output['rowCount'] = $stmt->rowCount();
-                        
-                        $output['success'] = true;
-                        $output['filename01'] = $filename01;
-                        $output['filename02'] = $filename02;
 
+                        if ($stmt->rowCount() == 0) {
+                            $output['error'] = '資料沒有修改';
+                        } else {
+                            $output['success'] = true;
+                        }
                     } else {
                         $output['error'] = '無法移動檔案02';
                     }
@@ -67,18 +68,41 @@ if (!empty($_FILES['container_img'])) {
             } else {
                 $output['error'] = '不合法的檔案類型02';
             }
-            
         } else {
             $output['error'] = '不合法的檔案類型01';
         }
-        
     } else {
         $output['error'] = '沒有上傳檔案02';
     }
-    
 } else {
     $output['error'] = '沒有上傳檔案01';
 }
 
-echo json_encode($output, JSON_UNESCAPED_UNICODE);
+if (empty($_FILES['container_img'])) {
+    $ext01 = $exts[$_FILES['container_img']['type']];
+    if (empty($_FILES['container_shadow'])) {
+        $ext02 = $exts[$_FILES['container_shadow']['type']];
 
+        $sql = "UPDATE `product_container` SET
+                            `container_name`=?,
+                            WHERE container_id=?";
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute([
+            $container_name,
+            $container_id
+        ]);
+
+        if ($stmt->rowCount() == 0) {
+            $output['error'] = '資料沒有修改';
+        } else {
+            $output['success'] = true;
+        }
+    } else {
+        $output['error'] = '沒有上傳檔案02';
+    }
+} else {
+    $output['error'] = '沒有上傳檔案01';
+}
+
+
+echo json_encode($output, JSON_UNESCAPED_UNICODE);
